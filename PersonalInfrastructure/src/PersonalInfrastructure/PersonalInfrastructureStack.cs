91266c83158966ad9969726cc931b480efb4e3cc
Scottie Enriquez <scottie@enriquez.xxx>
2022-12-14 19:45:42 -0800

    Adding initial skeleton

---
+++ PersonalInfrastructure/src/PersonalInfrastructure/PersonalInfrastructureStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace PersonalInfrastructure
+{
+    public class PersonalInfrastructureStack : Stack
+    {
+        internal PersonalInfrastructureStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}

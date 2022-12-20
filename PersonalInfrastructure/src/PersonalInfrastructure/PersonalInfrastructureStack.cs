75de27cc145252879bfa98595fc7da1d0f05881a
Scottie Enriquez <scottie@enriquez.xxx>
2022-12-19 19:33:31 -0600

    Adding Lambda function skeleton

--- PersonalInfrastructure/src/PersonalInfrastructure/PersonalInfrastructureStack.cs
+++ PersonalInfrastructure/src/PersonalInfrastructure/PersonalInfrastructureStack.cs
@@ -1,4 +1,5 @@
 using Amazon.CDK;
+using Amazon.CDK.AWS.Lambda;
 using Constructs;
 
 namespace PersonalInfrastructure
@@ -7,7 +8,16 @@ namespace PersonalInfrastructure
     {
         internal PersonalInfrastructureStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
         {
-            // The code that defines your stack goes here
+            DockerImageCode gamingPCShutdownLambdaDockerImageCode = DockerImageCode.FromImageAsset("src/PersonalInfrastructure.GamingPC.ScheduledShutdown.Lambda/src/PersonalInfrastructure.GamingPC.ScheduledShutdown.Lambda");
+            DockerImageFunction gamingPCShutdownLambda = new DockerImageFunction(this, "gamingPCShutdownLambda",
+                new DockerImageFunctionProps()
+                {
+                    Architecture = Architecture.ARM_64,
+                    Code = gamingPCShutdownLambdaDockerImageCode,
+                    Description = "Function to turn off gaming PC",
+                    Timeout = Duration.Seconds(30) 
+                }
+            );
         }
     }
 }
